f59a8cd009cde9c4a2a24b0bc6e402f89f47ec6d
Yassar Hammami <yassarhammami@gmail.com>
2022-05-27 02:52:09 +0300

    .

--- src/main/java/com/jazara/icu/registry/RegistryApplication.java
+++ src/main/java/com/jazara/icu/registry/RegistryApplication.java
@@ -3,9 +3,11 @@ package com.jazara.icu.registry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableEncryptableProperties
 public class RegistryApplication {
 
 	public static void main(String[] args) {
